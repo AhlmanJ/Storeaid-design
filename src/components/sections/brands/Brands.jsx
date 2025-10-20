405728c5809d6f1fa23eaa6d45328d9a5d3493c0
AhlmanJ <johan.ahlman@hotmail.com>
2025-10-20 14:05:58 +0200

    added the brands-section

---
+++ src/components/sections/brands/Brands.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Brands.css'
+
+function Brands() {
+  return (
+    <div className='brands-container'>
+      <img src="pictures/brands/Logoipsum1.svg" alt="" />
+      <img src="pictures/brands/Logoipsum2.svg" alt="" />
+      <img src="pictures/brands/Logoipsum3.svg" alt="" />
+      <img src="pictures/brands/Logoipsum4.svg" alt="" />
+      <img src="pictures/brands/Logoipsum5.svg" alt="" />
+    </div>
+  )
+}
+
+export default Brands
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../components/sections/hero/Hero'
 import About from '../components/sections/about-us/About'
+import Brands from '../components/sections/brands/Brands'
 
 function HomePage() {
   return (
@@ -8,6 +9,9 @@ function HomePage() {
         <Hero />
         <section className='aboutUs'>
           <About />
+        </section>
+        <section className='brands'>
+          <Brands />
         </section>
     </div>
   )
