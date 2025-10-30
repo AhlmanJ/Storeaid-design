07dcd2b4aec70e6c3ad4b29e6718e8fd3f8b0d8c
AhlmanJ <johan.ahlman@hotmail.com>
2025-10-30 14:03:23 +0100

    Added a new section - FAQ

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -3,6 +3,7 @@ import Banner from '../components/sections/banner/Banner'
 import WhyChooseUs from '../components/sections/whyChooseUs/WhyChooseUs'
 import LookingForSecure from '../components/sections/lookingForSecure/LookingForSecure'
 import Subscribe from '../components/sections/subscribe/Subscribe'
+import FAQ from '../components/sections/faq/FAQ'
 
 
 
@@ -15,6 +16,9 @@ const Booking = () => {
       <WhyChooseUs />
       <section className='looking'>
         <LookingForSecure />
+      </section>
+      <section className='faq'>
+        <FAQ />
       </section>
       <section className='subscribe'>
         <Subscribe />
