f2d61748af4bc8500ba838c9aa9e28a177631672
AhlmanJ <johan.ahlman@hotmail.com>
2025-10-31 17:55:09 +0100

    Added a new component Contact form

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Banner from '../components/sections/banner/Banner'
-import SubscribeForm from '../components/elements/SubscribeForm'
 import Subscribe from '../components/sections/subscribe/Subscribe'
 import FAQ from '../components/sections/faq/FAQ'
 import FindUsOn from '../components/sections/findUsOn/FindUsOn'
