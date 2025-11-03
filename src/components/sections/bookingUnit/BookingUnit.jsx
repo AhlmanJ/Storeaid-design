7627c6e01a6ba48855b219d22760e9f1a851efed
AhlmanJ <johan.ahlman@hotmail.com>
2025-11-03 16:32:15 +0100

    Added a new section - Booking unit

---
+++ src/components/sections/bookingUnit/BookingUnit.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './BookingUnit.css'
+import BookingForm from '../../elements/BookingForm'
+
+function BookingUnit() {
+  return (
+    <div className='bookingUnit-container'>
+        <div className='booking-leftSide'>
+            <h2 className='booking-h2'>Booking Unit</h2>
+            <h3 className='booking-h3'>Book Your Storage Unit Now & Simplify Your Life!</h3>
+            <div className='booking-ImgContainer'></div>
+        </div>
+        <div className='booking-rightSide'>
+            <p className='booking-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
+            <BookingForm />
+        </div>
+    </div>
+  )
+}
+
+export default BookingUnit
